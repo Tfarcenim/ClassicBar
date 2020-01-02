3cc1965df8b2b84b6dc9b7c3d96aa0c8ebeae00f
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-01-01 21:31:25 -0500

    Update NetworkHelper.java

--- src/main/java/tfar/classicbar/network/NetworkHelper.java
+++ src/main/java/tfar/classicbar/network/NetworkHelper.java
@@ -5,10 +5,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.fml.network.NetworkDirection;
 import net.minecraftforge.fml.network.NetworkEvent;
 
-public class NetworkHelper
-{
-    public static PlayerEntity getSidedPlayer(NetworkEvent.Context ctx)
-    {
-        return ctx.getDirection() == NetworkDirection.PLAY_TO_SERVER ? ctx.getSender() : Minecraft.getInstance().player;
-    }
+public class NetworkHelper {
+  public static PlayerEntity getSidedPlayer(NetworkEvent.Context ctx) {
+    return ctx.getDirection() == NetworkDirection.PLAY_TO_SERVER ? ctx.getSender() : Minecraft.getInstance().player;
+  }
 }
